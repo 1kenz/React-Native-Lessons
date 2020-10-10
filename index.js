5536f5f7bc8e83efd8550e2a090aa9b2d26e8b5e
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-10 12:04:29 +0300

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './__lesson__/session1/src/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
