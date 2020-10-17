c81492e906e4f4eb3dd942a2271a2ef5957320ff
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-18 02:18:43 +0300

    Create Main.js

---
+++ __lesson__/session7/src2/Main.js
@@ -0,0 +1,70 @@
+// Overall - 1
+import React, { useEffect, useState } from "react";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Alert,
+  TextInput,
+} from "react-native";
+import productData from "./product_data.json";
+import { ProductCard, SearchBar } from "./components";
+const App = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [displayList, setDisplayList] = useState([]);
+  const renderListItem = ({ item }) => <ProductCard product={item} />;
+  useEffect(() => {
+    // Alert.alert("Clarushop", "Hoşgeldiniz, keyifli alışverişler..");
+    setDisplayList(productData);
+  }, []);
+  useEffect(() => {
+    const filteredValue = productData.filter((item) => {
+      const text = searchValue.toUpperCase();
+      const productTitle = item.title.toUpperCase();
+      return productTitle.indexOf(text) > -1;
+    });
+    setDisplayList(filteredValue);
+  }, [searchValue]);
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        <Text style={styles.banner}>Clarushop</Text>
+        <SearchBar getName={(val) => setSearchValue(val)} />
+
+        {/* <View style={styles.searchBar}>
+          <TextInput
+            placeholder="Ürün ara..."
+            onChangeText={(value) => setSearchValue(value)}
+          />
+        </View> */}
+        <FlatList
+          keyExtractor={(_, index) => index.toString()}
+          data={displayList}
+          renderItem={renderListItem}
+          numColumns={2}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+/**
+ * 1) Stok durumunu belirtmeniz
+ * 2) <SearchBar onSearch={...} />
+ */
+export default App;
+const styles = StyleSheet.create({
+  banner: {
+    color: "purple",
+    fontWeight: "bold",
+    fontSize: 40,
+    textAlign: "center",
+  },
+  searchBar: {
+    backgroundColor: "#eceff1",
+    padding: 8,
+    margin: 5,
+    borderRadius: 10,
+  },
+});
