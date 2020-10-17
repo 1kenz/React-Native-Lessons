4085ff7f45cdcabde73bb05441d47a35804c8f74
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-18 02:18:07 +0300

    Create App.js

---
+++ __lesson__/session6/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { SafeAreaView, View, Text, Button } from 'react-native';
+
+
+const App = (props) => {
+  const [orderCount, setOrderCount] = useState(0);
+
+  useEffect(() => {
+    Alert.alert("CLARUSWAY", "Hoşgeldiniz")
+  }, [])
+
+  useEffect(() => {
+    if (orderCount > 10) {
+      Alert.alert("CLARUSWAY", "10'dan fazla seçim yapıyorsunuz.")
+    }
+  }, [orderCount])
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text style={{ fontSize: 50 }}>Count {orderCount}</Text>
+        <Button
+          title="Select Order"
+          onPress={() => setOrderCount(orderCount + 1)}
+        />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default App;
