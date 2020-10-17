0afb8fff119399a2ebbe3a7bf1683702e4fc1296
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-18 02:18:13 +0300

    Create Functional.js

---
+++ __lesson__/session6/src/Functional.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, View, Text, Button, Alert } from 'react-native';
+
+const App = (props) => {
+  const [orderCount, setOrderCount] = useState(0);
+
+  useEffect(() => {
+    Alert.alert("CLARUSWAY", "Hoşgeldiniz")
+  }, [])
+
+  useEffect(() => {
+    if (orderCount > 10) {
+      Alert.alert("CLARUSWAY", "10'dan fazla seçim yapıyorsunuz.")
+    }
+  }, [orderCount])
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text style={{ fontSize: 50 }}>Count {orderCount}</Text>
+        <Button
+          title="Select Order"
+          onPress={() => setOrderCount(orderCount + 1)}
+        />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default App;
