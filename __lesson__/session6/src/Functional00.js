b00cddda1099ee077d83e7da07e8b85d00bfb4f0
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-18 02:18:15 +0300

    Create Functional00.js

---
+++ __lesson__/session6/src/Functional00.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, View, Text, Button } from 'react-native';
+
+const App = (props) => {
+  const [counter, setCounter] = useState(0);
+  const [myNumber, setNumber] = useState(0);
+  const [createDate, setCreateDate] = useState(new Date());
+
+  useEffect(() => {
+    console.log("USE EFFECT[]");
+  }, [])
+
+  useEffect(() => {
+    console.log("useEffect counter!")
+  }, [counter])
+
+  useEffect(() => {
+    console.log("useEffect myNumber!")
+  }, [myNumber])
+
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text style={{ fontSize: 80 }}>Count: {counter}</Text>
+        <Button
+          title="up!"
+          onPress={() => setCounter(counter + 1)}
+        />
+        <Button
+          title="change number"
+          onPress={() => setNumber(myNumber + 2)}
+        />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default App;
