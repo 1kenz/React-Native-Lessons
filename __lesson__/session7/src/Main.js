3bfd5f275b700bc820559befef9906683c8d89b7
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-18 02:18:32 +0300

    Create Main.js

---
+++ __lesson__/session7/src/Main.js
@@ -0,0 +1,22 @@
+// Overall - 1
+import React from "react";
+import { SafeAreaView, View, Text, Alert } from "react-native";
+
+import MyInput from "./components/MyInput";
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text style={{ fontSize: 50 }}>Hello there</Text>
+
+        <MyInput
+          title="Set Name"
+          getName={(myValue) => alert("Girilen değer: " + myValue)}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
