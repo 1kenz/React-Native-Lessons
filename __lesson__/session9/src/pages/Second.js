5ac405459fae176f622df46f30e7740d1294c76f
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-20 22:30:15 +0300

    Update Second.js

--- __lesson__/session9/src/pages/Second.js
+++ __lesson__/session9/src/pages/Second.js
@@ -5,6 +5,7 @@ export default function Second(props) {
   return (
     <View>
       <Text style={{ fontSize: 40 }}>Second Page</Text>
+      <Button title="Back" onPress={() => props.navigation.goBack()} />
     </View>
   );
 }
