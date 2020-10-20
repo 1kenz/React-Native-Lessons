9dd1c9ed7fad6fe9e25a6aad19d386c4f9cd0445
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-20 22:00:10 +0300

    Update First.js

--- __lesson__/session9/src/pages/First.js
+++ __lesson__/session9/src/pages/First.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
 export default function First(props) {
   return (
     <View>
-      <Text>First Page</Text>
+      <Text style={{ fontSize: 40 }}>First Page</Text>
+      <Button
+        title="Second Page"
+        onPress={() => props.navigation.navigate("SecondPage")}
+      />
     </View>
   );
 }
