1316e25bb20af198c3c03995c9167cb29ccba37b
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-20 23:02:36 +0300

    Update First.js

--- __lesson__/session9/src/pages/First.js
+++ __lesson__/session9/src/pages/First.js
@@ -1,13 +1,24 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button, TextInput } from "react-native";
 
 export default function First(props) {
+  const [userName, setUserName] = useState("");
+  const number = 22;
+
   return (
     <View>
-      <Text style={{ fontSize: 40 }}>First Page</Text>
+      <Text style={{ fontSize: 40 }}>First</Text>
+      <Text style={{ fontSize: 40 }}>{number}</Text>
+      <TextInput value={userName} onChangeText={(text) => setUserName(text)} />
       <Button
         title="Second Page"
-        onPress={() => props.navigation.navigate("SecondPage")}
+        onPress={() =>
+          props.navigation.navigate("SecondPage", {
+            myNumber: number,
+            name: "ken",
+            selectedValue: userName,
+          })
+        }
       />
     </View>
   );
