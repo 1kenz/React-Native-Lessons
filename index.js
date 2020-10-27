5756f9004a2ad903e7271b544681d8e1f8b09722
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-27 23:03:03 +0300

    start

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import "react-native-gesture-handler";
 import { AppRegistry } from "react-native";
-import App from "./__lesson__/session11/src/Main2";
+import App from "./__lesson__/session12/src/Main";
 import { name as appName } from "./app.json";
 
 AppRegistry.registerComponent(appName, () => App);
