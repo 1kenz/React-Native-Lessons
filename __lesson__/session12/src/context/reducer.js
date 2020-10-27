5904f47c91b269ea55392f8e44b9ae2b1d0132c2
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-27 23:23:09 +0300

    Update reducer.js

--- __lesson__/session12/src/context/reducer.js
+++ __lesson__/session12/src/context/reducer.js
@@ -1,5 +1,15 @@
-function reducer (state, action){
-  switch(action.type){
-    
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREASE_COUNTER":
+      state.counter = state.counter + 1;
+      return { ...state };
+
+    case "DECREASE_COUNTER":
+      state.counter = state.counter - 1;
+      return { ...state };
+    // return {...state, state.counter -1}  // tek satırda işlem
+
+    default:
+      return state;
   }
 }
