8a686427688e316ae90cbb278b4a72c68eb77976
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-27 23:37:09 +0300

    provider created

--- __lesson__/session12/src/pages/Component_A.js
+++ __lesson__/session12/src/pages/Component_A.js
@@ -3,6 +3,8 @@ import { View, Text, Button } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 
 const Component_A = (props) => {
+  const myCounter = useSelector((globalState) => globalState.counter);
+  const dispatch = useDispatch();
 
   return (
     <View style={{ backgroundColor: "#eceff1", flex: 1 }}>
