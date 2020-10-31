4ba1e189961a8447aa0d9001f33ff9c0f6e6e3dc
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-31 18:23:33 +0300

    Create Router.js

---
+++ __lesson__/session14/src/Router.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { SafeAreaView, View, Text } from "react-native";
+
+import { Favorites, Restaurants } from "./pages";
+
+const Router = (props) => {
+  return (
+    <View>
+      <Text>Router</Text>
+    </View>
+  );
+};
+
+export default Router;
