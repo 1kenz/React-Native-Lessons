f1602c1479e4881ae28b58f45da48b52db799582
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-31 19:17:24 +0300

    Update Restaurants.js

--- __lesson__/session14/src/pages/Restaurants.js
+++ __lesson__/session14/src/pages/Restaurants.js
@@ -1,7 +1,33 @@
-import React from "react";
-import { SafeAreaView, View, Text } from "react-native";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { SafeAreaView, Text, View } from "react-native";
 
 const Restaurants = (props) => {
+  const fetchData = () => {
+    axios
+      .post(
+        "https://worldwide-restaurants.p.rapidapi.com/search",
+        {
+          limit: "30",
+          language: "en_US",
+          location_id: "297704",
+          currency: "USD",
+        },
+        {
+          headers: {
+            "content-type": "application/json",
+            "x-rapidapi-host": "worldwide-restaurants.p.rapidapi.com",
+            "x-rapidapi-key":
+              "bf2e35a991mshde8353f3fc95d2ap125b90jsn29120143a9d7",
+          },
+        },
+      )
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => fetchData(), []);
+
   return (
     <SafeAreaView>
       <View>
