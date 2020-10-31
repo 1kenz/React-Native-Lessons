930442e7eb38191dcedd7423815fcc5f7520b71e
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-31 18:23:30 +0300

    Create Restaurants.js

---
+++ __lesson__/session14/src/pages/Restaurants.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { SafeAreaView, View, Text } from "react-native";
+
+const Restaurants = (props) => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Restaurants</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export { Restaurants };
