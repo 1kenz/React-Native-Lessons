faf414c5886eeba5490901883a14bfde0db34969
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-31 21:18:38 +0300

    Update Restaurants.js

--- __lesson__/session14/src/pages/Restaurants.js
+++ __lesson__/session14/src/pages/Restaurants.js
@@ -7,36 +7,36 @@ import { RestaurantItem } from "../components";
 const Restaurants = (props) => {
   const [list, setList] = useState([]);
 
-  // const fetchData = () => {
-  //   Axios
-  //     .post(
-  //       "https://worldwide-restaurants.p.rapidapi.com/search",
-  //       {
-  //         limit: "30",
-  //         language: "en_US",
-  //         location_id: "297704",
-  //         currency: "USD",
-  //       },
-  //       {
-  //         headers: {
-  //           "content-type": "application/json",
-  //           "x-rapidapi-host": "worldwide-restaurants.p.rapidapi.com",
-  //           "x-rapidapi-key":
-  //             "bf2e35a991mshde8353f3fc95d2ap125b90jsn29120143a9d7",
-  //         },
-  //       },
-  //     )
-  //     .then((response) => setList(response.data.results.data))
-  //     .catch((error) => console.log(error));
-  // };
+   const fetchData = () => {
+    Axios
+      .post(
+        "https://worldwide-restaurants.p.rapidapi.com/search",
+        {
+          limit: "30",
+          language: "en_US",
+          location_id: "297704",
+          currency: "USD",
+        },
+        {
+          headers: {
+            "content-type": "application/json",
+            "x-rapidapi-host": "worldwide-restaurants.p.rapidapi.com",
+            "x-rapidapi-key":
+              "bf2e35a991mshde8353f3fc95d2ap125b90jsn29120143a9d7",
+          },
+        },
+      )
+      .then((response) => setList(response.data.results.data))
+      .catch((error) => console.log(error));
+  };
 
-  // useEffect(() => fetchData(), []);
+  useEffect(() => fetchData(), []);
 
   const renderList = ({ item }) => <RestaurantItem item={item} />;
 
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
         <Text style={{ fontSize: 25, textAlign: "center", fontWeight: "bold" }}>
           Restaurants
         </Text>
@@ -44,6 +44,9 @@ const Restaurants = (props) => {
           keyExtractor={(_, index) => index.toString()}
           data={list}
           renderItem={renderList}
+          ItemSeparatorComponent={() => (
+            <View style={{ borderWidth: 0.5, borderColor: "#bdbdbd" }} />
+          )}
         />
       </View>
     </SafeAreaView>
