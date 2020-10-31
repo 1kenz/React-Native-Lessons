fd38b1cda86e4444b8309914d5bdf219a014246e
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-31 22:14:06 +0300

    Update RestaurantItem.js

--- __lesson__/session14/src/components/RestaurantItem.js
+++ __lesson__/session14/src/components/RestaurantItem.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-const RestaurantItem = ({ item }) => {
-    return (
-        <TouchableOpacity>
-            <View style={styles.container}>
-                <Text style={styles.text}>{item.name}</Text>
-            </View>
-        </TouchableOpacity>
-    )
-}
+const RestaurantItem = ({ item, onAddFavorite }) => {
+  return (
+    <TouchableOpacity style={styles.container} onPress={onAddFavorite}>
+      <Icon name={"fire"} size={20} color={"red"} />
+      <Text style={styles.text}>{item.name}</Text>
+    </TouchableOpacity>
+  );
+};
 
-export { RestaurantItem }
+export { RestaurantItem };
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 10,
-        margin: 5,
-        borderRadius: 5
-    },
-    text: {
-        fontWeight: '200'
-    }
-})
+  container: {
+    padding: 10,
+    margin: 5,
+    borderRadius: 5,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  text: {
+    marginLeft: 10,
+    fontWeight: "200",
+  },
+});
