af943a20f5a799da68fde2778caad86ebb34c703
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-31 19:17:22 +0300

    Update Router.js

--- __lesson__/session14/src/Router.js
+++ __lesson__/session14/src/Router.js
@@ -1,14 +1,21 @@
-import React from "react";
-import { SafeAreaView, View, Text } from "react-native";
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import { Favorites, Restaurants } from "./pages";
 
-const Router = (props) => {
+const Tab = createBottomTabNavigator();
+
+function Router() {
   return (
-    <View>
-      <Text>Router</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Favorite" component={Favorites} />
+        <Tab.Screen name="Restaurant" component={Restaurants} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-};
+}
 
 export default Router;
