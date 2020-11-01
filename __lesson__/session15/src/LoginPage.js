33ecd648462a82eea48e14c727c5805696785d7e
xkendx <63884610+xkendx@users.noreply.github.com>
2020-11-01 22:14:30 +0300

    Create LoginPage.js

---
+++ __lesson__/session15/src/LoginPage.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { SafeAreaView, View, Text, TextInput, Button } from "react-native";
+import auth from "@react-native-firebase/auth";
+
+const LoginPage = (props) => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const signIn = () => {
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error.code);
+      });
+  };
+
+  const signUp = () => {
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((response) => {
+        //uniqe id
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text style={{ fontWeight: "bold", fontSize: 20, textAlign: "center" }}>
+          Login
+        </Text>
+
+        <View
+          style={{
+            backgroundColor: "#eceff1",
+            margin: 5,
+            borderRadius: 5,
+          }}>
+          <TextInput
+            placeholder="Type your mail.."
+            onChangeText={(text) => setEmail(text)}
+          />
+        </View>
+
+        <View
+          style={{
+            backgroundColor: "#eceff1",
+            margin: 5,
+            borderRadius: 5,
+          }}>
+          <TextInput
+            placeholder="Type your password"
+            onChangeText={(text) => setPassword(text)}
+          />
+        </View>
+
+        <Button title="Sign Up" onPress={signUp} />
+        <Button title="Sign In" onPress={signIn} />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default LoginPage;
