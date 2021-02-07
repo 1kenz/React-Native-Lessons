bfa76419c031acaf1fa26aec4d9fa4e1fb95fae9
xkendx <xkendx@users.noreply.github.com>
2021-02-07 20:12:10 +0100

    feat: create styles.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @format
  */
 import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './__lesson__/session01/src/App.js';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
