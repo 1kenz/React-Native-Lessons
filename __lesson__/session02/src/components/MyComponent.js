c72e96a731115cc733366c13e6a97454a63eb12f
xkendx <xkendx@users.noreply.github.com>
2021-02-09 23:17:39 +0100

    feat: add styles.js

--- __lesson__/session02/src/components/MyComponent.js
+++ __lesson__/session02/src/components/MyComponent.js
@@ -1,8 +1,10 @@
-import React from "react";
-import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import styles from './styles';
 
 const MyComponent = () => {
-  return(
+  return (
     <View style={styles.container}>
       <Text style={styles.text}>Selamlar!</Text>
       <Text style={styles.text}>Clarusway App'e hoşgeldiniz!</Text>
@@ -11,22 +13,7 @@ const MyComponent = () => {
         <Text style={styles.text}>Fullstack Developer olmaya hazır olun!</Text>
       </View>
     </View>
-
-  )
-}
+  );
+};
 
 export default MyComponent;
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "red",
-    margin:5,
-    padding: 10,
-    borderColor:"#e0e0e0",
-    borderWidth:2,
-    borderRadius:10,
-  },
-  text: {
-    color:"white",
-  }
-});
