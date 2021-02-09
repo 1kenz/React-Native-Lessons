099f5bb2a3db92dcf7aadd9250230cb144f01bc3
xkendx <xkendx@users.noreply.github.com>
2021-02-09 23:17:44 +0100

    feat: add styles.js

---
+++ __lesson__/session02/src/components/styles.js
@@ -0,0 +1,15 @@
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'red',
+    margin: 5,
+    padding: 10,
+    borderColor: '#e0e0e0',
+    borderWidth: 2,
+    borderRadius: 10,
+  },
+  text: {
+    color: 'white',
+  },
+});
+
+export default styles;
