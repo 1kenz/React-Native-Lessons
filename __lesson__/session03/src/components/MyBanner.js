e34130ca11bbd27c314d65a95abb523b5f1f9912
xkendx <xkendx@users.noreply.github.com>
2021-02-10 21:11:12 +0100

    feat: add styles

--- __lesson__/session03/src/components/MyBanner.js
+++ __lesson__/session03/src/components/MyBanner.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+
+import styles from './Styles';
+
 const MyBanner = (props) => {
   // function chechActiveStatus(){
   //     if (props.isActive){
@@ -11,7 +14,7 @@ const MyBanner = (props) => {
   return (
     <View style={[styles.container, { backgroundColor: props.color }]}>
       <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <View style={{ flex: 1, }}>
+        <View style={{ flex: 1 }}>
           <Text style={styles.text}>{props.title}</Text>
           <Text>{props.desc}</Text>
         </View>
@@ -23,16 +26,3 @@ const MyBanner = (props) => {
   );
 };
 export default MyBanner;
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'gray',
-    padding: 10,
-    margin: 10,
-    borderRadius: 5,
-    alignItems: 'center'
-  },
-  text: {
-    fontWeight: 'bold',
-    fontSize: 20,
-  },
-});
