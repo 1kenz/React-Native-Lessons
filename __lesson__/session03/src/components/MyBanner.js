213feadc0af78353e83406abc7604049e6d1965b
xkendx <xkendx@users.noreply.github.com>
2021-02-11 21:36:38 +0100

    feat: add styles

--- __lesson__/session03/src/components/MyBanner.js
+++ __lesson__/session03/src/components/MyBanner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-import styles from './Styles';
+import styles from './MyBanner.styles';
 
 const MyBanner = (props) => {
   // function chechActiveStatus(){
