75ce92b6c09925c3b34827be0ff15eb613add04a
xkendx <xkendx@users.noreply.github.com>
2021-02-11 21:36:32 +0100

    feat: add styles

---
+++ __lesson__/session03/src/App.sytles.js
@@ -0,0 +1,13 @@
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#0277bd',
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+  },
+  text: {
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
