aca3a1b396b75be55a46cd5feeba124fe6673d2d
xkendx <xkendx@users.noreply.github.com>
2021-02-14 00:00:28 +0100

    feat: add styles.js

--- __lesson__/session07/src/components/InputPanel.js
+++ __lesson__/session07/src/components/InputPanel.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   TextInput,
   View,
   TouchableOpacity,
   Text,
   StyleSheet,
-} from "react-native";
+} from 'react-native';
+
+import styles from './InputPanel.styles.js';
+
 const InputPanel = (props) => {
-  const [inputText, setInputText] = useState("");
+  const [inputText, setInputText] = useState('');
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
@@ -19,29 +22,9 @@ const InputPanel = (props) => {
       <TouchableOpacity
         style={styles.buttonContainer}
         onPress={() => props.sendText(inputText)}>
-        <Text style={{ textAlign: "center" }}>Seç</Text>
+        <Text style={{ textAlign: 'center' }}>Seç</Text>
       </TouchableOpacity>
     </View>
   );
 };
 export default InputPanel;
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#cfd8dc",
-    padding: 10,
-    margin: 10,
-    borderRadius: 5,
-  },
-  inputContainer: {
-    backgroundColor: "#eceff1",
-    padding: 10,
-    margin: 10,
-    borderRadius: 5,
-  },
-  buttonContainer: {
-    backgroundColor: "#b2dfdb",
-    padding: 10,
-    margin: 10,
-    borderRadius: 10,
-  },
-});
