68849b53cce4abf8d712aaebd7ca6bc6d23919ac
xkendx <xkendx@users.noreply.github.com>
2021-02-14 00:00:17 +0100

    feat: add styles.js

---
+++ __lesson__/session07/src/components/Button.styles.js
@@ -0,0 +1,14 @@
+const Styles = StyleSheet.create({
+  buttonContainer: {
+    backgroundColor: '#a5d6a7',
+    padding: 15,
+    margin: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  textStyle: {
+    fontSize: 20,
+  },
+});
+
+export default Styles;
